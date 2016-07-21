02c2ffa6d0250a84db2367ef6e0df257bc645ee5
Sander van de Graaf <mail@svdgraaf.nl>
2016-07-21 13:32:08 +0200

    added extra formatting + option parser

--- runner.py
+++ runner.py
@@ -1,22 +1,33 @@
 import yaml
 import subprocess
 import sys
+from optparse import OptionParser
 
-filename = '.pipeline.yml'
-branch = 'default'
 
-with open(filename, 'r') as pipeline_config:
+parser = OptionParser()
+parser.add_option("-f", "--file", dest="filename", default='.pipeline.yml',
+                  help="Parse pipeline from FILE", metavar="FILE")
+parser.add_option("-b", "--branch", dest="branch", default='default',
+                  help="Parse pipeline for branch BRANCH", metavar="BRANCH")
+
+(options, args) = parser.parse_args()
+
+with open(options.filename, 'r') as pipeline_config:
     pipeline = yaml.load(pipeline_config)
 
     steps = []
 
-    if branch in pipeline['pipelines'].keys():
-        steps = pipeline['pipelines'][branch]
+    if options.branch in pipeline['pipelines'].keys():
+        steps = pipeline['pipelines'][options.branch]
+        print "Starting pipeline for {}".format(options.branch)
     else:
+        print "Starting pipeline for Default"
         steps = pipeline['pipelines']['default']
 
     for step in steps:
-        print "- {}".format(step['name'])
+        print "==============================="
+        print "== Step: {}".format(step['name'])
+        print "==============================="
 
         p = subprocess.Popen(step['script'], shell=True, stdout=subprocess.PIPE,
                                   stderr=subprocess.PIPE,
@@ -29,5 +40,5 @@ with open(filename, 'r') as pipeline_config:
         if p.returncode != 0:
             sys.exit(p.returncode)
 
-print "DONE"
+print "Pipeline DONE"
 sys.exit(0)
